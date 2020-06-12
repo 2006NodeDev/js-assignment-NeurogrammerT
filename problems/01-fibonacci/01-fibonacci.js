472cc284216e51337104f4da34b29d09d2dcd7d0
NeurogrammerT <bbstarnique@aol.com>
2020-06-12 18:23:30 -0400

    first attempt

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,15 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
+
+let n = 10
+
+let numA = fib(n - 1);
+
+let numB = fib(n - 2);
+
 function fib(n) {
 
-}
+    if (n < 1) return console.log('Your number is less than 1.');
+    
+} return console.log('The number is ' + (numA + numB) + '!');
