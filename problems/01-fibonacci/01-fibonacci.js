35700462634cc20a4e802e4cf8d5e914075f8d4e
NeurogrammerT <bbstarnique@aol.com>
2020-06-15 15:50:54 -0400

    fibonacci

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -4,7 +4,11 @@ Return the nth number in the fibonacci sequence. */
 
 function fib(n) {
 
-    if (n <= 1) return 1;
+    if (n <= 0) return console.log("This number is invalid!");
+
+    if (n === 1) return 0;
+
+    if (n === 2) return 1;
     
     return fib(n - 1) + fib(n - 2);   
 }
