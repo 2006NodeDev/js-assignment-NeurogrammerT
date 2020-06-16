f7d9d61b82bf4e72c4d1c7d234e10cd33a3be6d5
NeurogrammerT <bbstarnique@aol.com>
2020-06-16 14:47:44 -0400

    object creation

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,25 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let literalObj = { name: "Literal", Age: 21 };
+
+let constObj = new Object();
+
+constObj.name = "Constructor";
+
+constObj.age = 28;
+
+es6 = Object.create(literalObj);
+
+es6.name = "ES6";
+
+es6.age = 5;
+
+console.log(literalObj);
+
+console.log(constObj);
+
+console.log(es6);
+
+
