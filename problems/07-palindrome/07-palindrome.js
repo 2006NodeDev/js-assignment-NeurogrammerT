ea2f47783ebc48c9f7eaac63a175754e1ea68225
NeurogrammerT <bbstarnique@aol.com>
2020-06-16 12:46:53 -0400

    palindrome

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -1,6 +1,42 @@
 /* 7. Palindrome
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
+ 
+const stripLower = (someStr) => someStr.toLowerCase().replace(/[\W_]/g, '');
+
+// Loop Method
 function isPalindrome(someStr) {
   
+    const strippedString = stripLower(someStr);
+
+    for (let i = 0; i < strippedString.length / 2; i++) {
+        
+        if (strippedString[i] != strippedString[strippedString.length - 1 - i]) {
+            
+            return false;
+        }
+        
+    }
+
+    return true;
+
 }
+
+// JS Inbuilt method
+// function isPalindrome(someStr) {
+  
+//     const strippedString = stripLower(someStr);
+
+//     const reverseString = strippedString.split('').reverse().join('');
+
+//     return reverseString == strippedString;
+
+// }
+
+console.log(isPalindrome('racecar'));
+
+console.log(isPalindrome('Racecar'));
+
+console.log(isPalindrome("racecar!"));
+
+console.log(isPalindrome('carrot'));
