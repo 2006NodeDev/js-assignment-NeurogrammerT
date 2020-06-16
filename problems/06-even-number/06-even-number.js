76695756ff708a950a9b7a8f06a7ddcfd49198c1
NeurogrammerT <bbstarnique@aol.com>
2020-06-16 11:54:03 -0400

    even number

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -2,6 +2,16 @@
 Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
+
 function isEven(someNum) {
   
+    if((someNum & 1) == 0){
+        return true
+      }
+      return false;
 }
+
+console.log(isEven(9));
+
+console.log(isEven(6));
+
