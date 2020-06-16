881a45c85d70b4382803b6c1b754782ffd53bbc1
NeurogrammerT <bbstarnique@aol.com>
2020-06-16 14:55:35 -0400

    splice element

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,13 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
+
+arr = ["Dog", 50, "Apple", 45, "Garfield", 100, "Racecar", 3000];
+
 function spliceElement(someArr, index) {
   
+    return someArr.splice(index);
 }
+
+console.log(spliceElement(arr, 4));
+
